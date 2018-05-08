c2f1fb48138f50489daf3b9b87ef129908c003ef
Luis Quinones <luis@complicitMatter.com>
2018-05-08 14:27:19 -0700

    feat(main): add file stubs to Support

---
+++ GlosaNet.Rhino/Support/PointSupport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlosaNet.Rhino.Support
+{
+    public static class PointSupport
+    {
+    }
+}
