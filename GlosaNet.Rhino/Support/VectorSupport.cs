2753b1f39c5780281f847cff9a4ebb8cee48f5f1
Luis Quinones <luis@complicitMatter.com>
2018-05-08 14:30:22 -0700

    feat(main): add conversion methods to GlosaVector2 from Rhino Vector2s

--- GlosaNet.Rhino/Support/VectorSupport.cs
+++ GlosaNet.Rhino/Support/VectorSupport.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Glosa.Net.Core.Geometry;
+using Rhino.Geometry;
 
 namespace GlosaNet.Rhino.Support
 {
+    /// <summary>
+    /// Represents a static class for GlosaVector and Rhino Vector interactions
+    /// </summary>
     public static class VectorSupport
     {
+        /// <summary>
+        /// Creates a GlosaVector2 from a Rhino Vector2d
+        /// </summary>
+        /// <param name="rhinoVector">The Rhino Vector2d</param>
+        /// <returns>A new GlosaVector2 with the two, X and Y, components from the Rhino Vector2d</returns>
+        public static GlosaVector2 ToGlosaVector2(Vector2d rhinoVector)
+        {
+            return new GlosaVector2(rhinoVector.X, rhinoVector.Y);
+        }
+
+        /// <summary>
+        /// Creates a GlosaVector2 from a Rhino Vector2f
+        /// </summary>
+        /// <param name="rhinoVector">The Rhino Vector2f</param>
+        /// <returns>A new GlosaVector2 with the two, X and Y, components from the Rhino Vector2f</returns>
+        public static GlosaVector2 ToGlosaVector2(Vector2f rhinoVector)
+        {
+            return new GlosaVector2(rhinoVector.X, rhinoVector.Y);
+        }
     }
 }
